4a1934b194e6baa4cd4290ee5e29383858878ae9
KevinChang <apog780212@gmail.com>
2020-11-25 16:29:02 +0800

    gulp第五週作業修改

--- week5/app/assets/js/all.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.button').click(function (event) {
-        $('.text').toggleClass('active');
-        //$('.text').addClass('active');
-        //$('.text').removeClass('active');
-    });
-});
